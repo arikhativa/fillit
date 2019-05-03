6735671a3d1be6795795dbda4740f58298e97370
yoav rabby <yoavar@protonmail.com>
2019-05-03 14:05:24 +0200

    changes in Makefile of libft

---
+++ fillit/includs/fillit.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrabby <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/30 15:34:48 by yrabby            #+#    #+#             */
+/*   Updated: 2019/04/30 15:50:10 by yrabby           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include "libft.h"
+
+#endif
